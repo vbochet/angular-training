e4285b54af192cd696c7c2e7f4314e4fa67bda55
Vincent Bochet <vbochet@excilys.com>
2018-05-15 17:58:19 +0200

    Add animations

--- src/app/recipe/recipes/recipes.component.ts
+++ src/app/recipe/recipes/recipes.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { trigger, transition, query, style, stagger, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.scss']
+  styleUrls: ['./recipes.component.scss'],
+
+  animations: [
+    trigger('explainerAnim', [
+      transition('* => *', [
+        query('.col', style({ opacity: 0, transform: 'translateX(-40px)' }), {optional: true}),
+
+        query('.col', stagger('500ms', [
+          animate('800ms 1.2s ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
+        ]), {optional: true}),
+
+        query('.col', [
+          animate(1000, style('*'))
+        ], {optional: true})
+
+      ])
+    ])
+  ]
 })
 export class RecipesComponent implements OnInit {
 
